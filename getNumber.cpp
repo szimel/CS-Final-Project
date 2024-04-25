9f329b6e4b8c529976fd0015499e4326581309ca
Samuel Zimel <samuel.c.zimel@gmail.com>
2024-04-25 12:50:19 -0500

    Initial commit

---
+++ getNumber.cpp
@@ -0,0 +1,24 @@
+#include "euid_header.h"
+int getNumber()
+{
+	ifstream fin;
+	fin.open("student.dat");
+	if (fin.fail())
+	{
+		cout<<"File error."<<endl;
+		exit(1);
+	}
+	int count=0;
+	string line;
+	while(!fin.eof())
+	{
+		getline(fin, line);
+		++count;
+	}
+	
+	fin.close();
+	
+	cout << "Number of students = " << count << endl;
+	return(count);
+	
+}
